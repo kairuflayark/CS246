c9cc53cb6051f28a8cd179c30ca25244ef9eae61
macbeth-byui <31939230+macbeth-byui@users.noreply.github.com>
2018-05-23 11:19:59 -0600

    initial

---
+++ HelloWorldApp/app/src/main/java/macbeth/HelloWorldApp/MainActivity.java
@@ -0,0 +1,22 @@
+package macbeth.HelloWorldApp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayHello(View view) {
+        //TextView tv = findViewById(R.id.helloTextView);
+        //tv.setText("Hello!");
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
